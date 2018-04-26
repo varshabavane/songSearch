dfa6fbc53319fff941076fc9f9639c3265f4e9c7
varsha bavane <varshab13bavane@gmail.com>
2018-04-26 21:30:13 +0530

    component generated for showing progressbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { StatusBar } from "@ionic-native/status-bar";
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { MyApp } from "./app.component";
+import { HomePage } from "../pages/home/home";
+import { ProgressBarComponent } from "../components/progress-bar/progress-bar";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
+  declarations: [MyApp,
+     HomePage, 
+     ProgressBarComponent],
   imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
+    BrowserModule, 
+    IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: [MyApp, HomePage],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,147 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  music: MediaObject;
 
-  constructor(public navCtrl: NavController) {
+  nativePath: string;
 
+  tracks: any;
+  playing: boolean;
+  currentTrack: any;
+  progressInterval: any;
+  constructor(
+    public navCtrl: NavController,
+    private fileChooser: FileChooser,
+    // private filePath: FilePath,
+    private media: Media
+  ) {
+    this.tracks = [
+      {
+        title: "hrudyat vaje smthng",
+        artist: "rohit raut",
+        playing: false,
+        songPath: "/android_asset/www/assets/songs/hrudyat vaje smthng.mp3",
+        progress:0,
+        img: "/android_asset/www/assets/imgs/2.jpg"
+      },
+      {
+        title: "love u zindagi",
+        artist: "alia",
+        playing:false,
+        songPath: "/android_asset/www/assets/songs/lvuZindgi.mp3",
+        progress: 0,
+        img: "/android_asset/www/assets/imgs/3.jpg"
+      },
+      {
+        title: "pahli najarme",
+        artist: "avdutgupte",
+        playing: false,
+        songPath: "/android_asset/www/assets/songs/pahlinajarme.mp3",
+        progress: 0,
+        img: "/android_asset/www/assets/imgs/2.jpg"
+      },
+      {
+        title: "Gannayka dj",
+        artist: "",
+        playing:false,
+        songPath: "/android_asset/www/assets/songs/gannayka.mp3",
+        progress: 0,
+        img: "/android_asset/www/assets/imgs/3.jpg"
+      },
+      {
+        title: "tik-tik",
+        artist: "atif aslam",
+        playing: false,
+        songPath: "/android_asset/www/assets/songs/tik-tik.mp3",
+        progress: 0,
+        img: "/android_asset/www/assets/imgs/4.jpg"
+      },
+      {
+        title: "sojajara",
+        artist: "papon",
+        playing: false,
+        songPath: "/android_asset/www/assets/songs/sojajara.mp3",
+        progress: 0,
+        img: "/android_asset/www/assets/imgs/1.jpg"
+      }
+    ];
+    this.currentTrack = this.tracks[0];
   }
 
+  filechooser() {
+    this.fileChooser
+      .open()
+      .then(uri => {
+        (<any>window).FilePath.resolveNativePath(
+          uri,
+          result => {
+            alert("result" + result);
+            alert("uri" + uri);
+            // this.audioPlay(result);
+            this.nativePath = result;
+            let pathalone = this.nativePath.substring(8);
+            let newSong = {
+              title: "aafreen",
+              artist: "sonu nigam",
+              playing: "false",
+              songPath: pathalone,
+              progress: "0",
+              img: "android/assets/imgs/1.mp3"
+            };
+            this.tracks.push(newSong);
+            alert(JSON.stringify(this.tracks));
+            //  this.playTrack()
+          },
+          err => {
+            alert(err);
+          }
+        );
+      })
+      .catch(e => console.log(e));
+  }
+  // audioPlay(res) {
+  //   this.nativePath = res;
+  //   alert(this.nativePath);
+  //   let pathalone = this.nativePath.substring(8);
+  //   alert("pathalone" + pathalone);
+  //   this.music.pause();
+  //   this.music = this.media.create(pathalone);
+
+  //   this.music.play();
+  // }
+
+  playTrack(track) {
+    for (let checktrack of this.tracks) {
+      if (checktrack.playing) {
+        this.pauseTrack(checktrack);
+      }
+    }
+    track.playing = true;
+    this.currentTrack = track;
+    this.music = this.media.create(this.currentTrack.songPath);
+    this.music.play();
+    this.progressInterval = setInterval(() => {
+     this.currentTrack.progress < 100 ? this.currentTrack.progress++ : (this.currentTrack.progress = 0);
+    }, 10000);
+  }
+
+  pauseTrack(track) {
+    track.playing = false;
+    this.music.pause();
+    clearInterval(this.progressInterval);
+  }
+
+  nextTrack() {
+    let index = this.tracks.indexOf(this.currentTrack);
+    index >= this.tracks.length - 1 ? (index = 0) : index++;
+    this.playTrack(this.tracks[index]);
+  }
+
+  prevTrack() {
+    let index = this.tracks.indexOf(this.currentTrack);
+    index > 0 ? index-- : (index = this.tracks.length - 1);
+    this.playTrack(this.tracks[index]);
+  }
 }
+
+  
